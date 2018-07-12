3e88273284556ad5471dbf11cef3d33eaf59a7c0
Alveena24G <41142244+Alveena24G@users.noreply.github.com>
2018-07-12 14:58:12 +0530

    Create alnew.py

---
+++ alnew.py
@@ -0,0 +1,8 @@
+alist=['a','e','i','o','u','b','c','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+alpha=input()
+if(alpha=='d'):
+ print("yes")
+elif(alpha in alist):
+	print("Alphabet")
+else:
+	print("No")
