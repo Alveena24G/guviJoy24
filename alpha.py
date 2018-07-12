92add8181f90dc5e3dc39802e6dc648635b9f973
Alveena24G <41142244+Alveena24G@users.noreply.github.com>
2018-07-12 14:53:26 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,6 @@
+alist=['a','e','i','o','u','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+alpha=input()
+if(alpha in alist):
+	print("Yes")
+else:
+	print("No")
